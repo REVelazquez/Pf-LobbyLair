bdcabe87eb488c745cde61e96cd1fe3f379c94ac
matias arroyo <matiasarroyo684@gmail.com>
2023-06-14 10:56:28 -0300

    rutas usuarios

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,22 +1,30 @@
 const { Router } = require('express');
-const userController = require('../controllers/userContoller')
+const { getGames, getGamesById } = require('../controllers/games.js');
+const { getPosts } = require('../controllers/post.js');
+const { getAllUsers, getUserById, getUserByName, createUser, updateUser, deleteUser} = require('../controllers/users.js');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
 const router = Router();
+
+router.get('/Games', getGames);
+router.get('/Games/:id', getGamesById);
+router.get('/Posts', getPosts);
 // Endpoint para obtener todos los usuarios
-router.get('/users/', userController.getAllUsers);
+router.get('/users/', getAllUsers);
 
 // Endpoint para obtener un usuario por id
-router.get('/users/:id', userController.getUserById);
+router.get('/users/:id', getUserById);
 
 // Endpoint para obtener un usuario por nombre
-router.get('/users/:name', userController.getUserByName);       
+router.get('/users/:name', getUserByName);       
 
 // Endpoint para crear un usuario
-router.post('/users/', userController.createUser);
+router.post('/users/', createUser);
 
 // Endpoint para actualizar un usuario
-router.put('/users/:id', userController.updateUser);
+router.put('/users/:id', updateUser);
 
 // Endpoint para eliminar un usuario
-router.delete('/users/:id', userController.deleteUser);
-
+router.delete('/users/:id', deleteUser);
 module.exports = router;
