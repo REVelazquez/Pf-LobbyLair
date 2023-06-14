fafdc7ccbdd5ecbe9dd57eb8bcbe3b7a53d2b037
matias arroyo <matiasarroyo684@gmail.com>
2023-06-14 10:53:01 -0300

    rutas usuarios

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -1,9 +1,128 @@
-const { User } = require('../db.js');
+const axios = require('axios');
+const { User } = require('../db');
+const { Op } = require("sequelize");
 
-async function getUsers (rea, res){
-  return res.status(200).json("a");
-}
 
+// Función para obtener todos los usuarios de la base de datos
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Error getting countries' });
+  }
+};
+// Función para obtener los detalles de un usuario por su ID
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      // Se busca el usuario en la base de datos por su ID
+      const user = await User.findOne({
+        where: {
+          id: {
+            [Op.iLike]: id,
+          },
+        },
+      });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Failed to get user' });
+    }
+  };
+  
+  // Función para buscar un usuario por su nombre
+  const getUserByName = async (req, res) => {
+    const { name } = req.params;
+    console.log("Name received from params:", name);
+    try {
+      // Se busca el usuario en la base de datos por su nombre
+      const user = await User.findAll({
+        where: {
+          name: {
+            [Op.iLike]: '%'+name+'%',
+          },
+        },
+      });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Error when searching for the user' });
+    }
+  };
+  const createUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    let new_user = null;
+    try{
+        new_user = await User.create({
+          name: name,
+          email: email,
+          password: password,
+          isAdmin: false, // Set a default value for isAdmin
+          perfilUrl: '', // Set a default value for perfilUrl
+          isPremium: false,
+        });
+        res.json(new_user);
+    } catch (error) {   
+        console.error(error);
+        return res.status(500).json({ message: 'Error creating user' });
+    }
+    };
+    const updateUser = async (req, res) => {
+        const { id } = req.params;
+        const { name, email, password } = req.body;
+        try{
+            await User.update(
+                {
+                    name: name,
+                    email: email,
+                    password: password,
+                    isAdmin: false, // Set a default value for isAdmin
+                    perfilUrl: '', // Set a default value for perfilUrl
+                    isPremium: false,
+                },
+                {
+                    where: {
+                        id: id,
+                    },
+                }
+            );
+            res.json({ message: 'User updated' });
+        } catch (error) {   
+            console.error(error);
+            return res.status(500).json({ message: 'Error updating user' });
+        }
+    };
+    const deleteUser = async (req, res) => {
+        const { id } = req.params;
+        try{
+            await User.destroy({
+                where: {
+                    id: id,
+                },
+            });
+            res.json({ message: 'User deleted' });
+        } catch (error) {   
+            console.error(error);
+            return res.status(500).json({ message: 'Error deleting user' });
+        }
+    };
+
+  
 module.exports = {
-  getUsers
+  getAllUsers,
+  getUserById,
+  getUserByName,
+  createUser,
+  updateUser,
+  deleteUser
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { getGames, getGamesById } = require('../controllers/games.js');
 const { getPosts } = require('../controllers/post.js');
-const { getUsers } = require('../controllers/users.js');
+const { getAllUsers, getUserById, getUserByName, createUser, updateUser, deleteUser} = require('../controllers/users.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -10,6 +10,21 @@ const router = Router();
 router.get('/Games', getGames);
 router.get('/Games/:id', getGamesById);
 router.get('/Posts', getPosts);
-router.get('/Users', getUsers);
+// Endpoint para obtener todos los usuarios
+router.get('/users/', getAllUsers);
 
+// Endpoint para obtener un usuario por id
+router.get('/users/:id', getUserById);
+
+// Endpoint para obtener un usuario por nombre
+router.get('/users/:name', getUserByName);       
+
+// Endpoint para crear un usuario
+router.post('/users/', createUser);
+
+// Endpoint para actualizar un usuario
+router.put('/users/:id', updateUser);
+
+// Endpoint para eliminar un usuario
+router.delete('/users/:id', deleteUser);
 module.exports = router;
