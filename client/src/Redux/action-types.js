8acba07aea511083a8c0786307d80a0df3d9cdaa
matias arroyo <matiasarroyo684@gmail.com>
2023-06-15 10:31:44 -0300

    reducer y actions usuarios

--- client/src/Redux/action-types.js
+++ client/src/Redux/action-types.js
@@ -1,4 +1,6 @@
 export const GET_ALL_GAMES = 'GET_ALL_GAMES';
 export const GET_GAMES_BY_NAME = 'GET_GAMES_BY_NAME';
 export const GET_GAME_BY_ID = 'GET_GAME_BY_ID';
-export const POST_GAME = 'POST_GAME'
+export const POST_GAME = 'POST_GAME';
+export const GET_USER_BY_ID = 'GET_USER_BY_ID';
+export const GET_USER_BY_EMAIL = 'GET_USER_BY_EMAIL';
