91edbb2ca2b14235169cb63934df5f2cccccc20c
Fervolpato <fervolpato1991@gmail.com>
2023-06-16 10:35:04 -0300

    Cambiando SearchBar

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from 'axios';
+import glass from '../../magnifying-glass-search.png';
 
 const SearchBar = () => {
 
@@ -16,12 +17,12 @@ const SearchBar = () => {
     }
    return (
     <div className="flex relative">
-        <input type='search' value={searchName.name} onChange={handleChange}  className="rounded-full mr-2 text-center placeholder-center font-semibold"/>
-        <button className="text-white text-lg font-semibold no-underline hover:text-gray-500">Search Game</button>
-        {searchName.name.length !== 0 && (<div className="absolute w-full top-8 right-14 bg-white bg-opacity-6 rounded-lg grid grid-cols-3 gap-x-1">
+        <input placeholder="Search game..." type='search' value={searchName.name} onChange={handleChange}  className="rounded-full mr-2 text-center placeholder-center font-semibold"/>
+        <button className="text-white text-lg font-semibold no-underline hover:text-gray-500"><img src={glass} alt=""/></button>
+        {searchName.name.length !== 0 && (<div className="absolute w-full top-8 right-14.2 bg-white bg-opacity-6 rounded-lg grid grid-cols-1 gap-x-1">
         {game ? (
               game.map((match) => (
-              <button className="text-black italic font-bold text-sm pr-4 pt-4">
+              <button className="text-black italic font-bold text-sm pr-4 pt-4 hover:bg-black hover:text-white text-center">
                 {match.name}
               </button>
             ))
