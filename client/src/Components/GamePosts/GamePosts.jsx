96984871d33cf20b780a13098d034c2dc79ff7ad
Lucas Ledesma <lucasfidel99@gmail.com>
2023-06-16 17:24:22 -0300

    a

--- client/src/Components/GamePosts/GamePosts.jsx
+++ client/src/Components/GamePosts/GamePosts.jsx
@@ -1,70 +1,55 @@
 import React, { useState } from "react";
-import GamesBar from "../GamesBar/GamesBar";
 import "./GamePosts.module.css";
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import { useSelector } from "react-redux";
 
 const GamePosts = () => {
-  const [postContent, setPostContent] = useState("");
-  const [posts, setPosts] = useState([]);
-  const [selectedGame, setSelectedGame] = useState(null);
+  const location = useLocation();
+  const { gameId, gameModeId } = queryString.parse(location.search);
+  const stateUser = useSelector((state) => state.user);
 
-  const handlePostContentChange = (event) => {
-    setPostContent(event.target.value);
-  };
+  const [userId, setUserId] = useState(stateUser.id);
+  const [text, setText] = useState("");
+  const [gameid, setGameid] = useState(gameId);
+  const [gamemodeid, setGamemodeid] = useState(gameModeId);
 
-  const handlePostSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if (selectedGame) {
-      const newPost = {
-        id: Math.random().toString(),
-        content: postContent,
-        gameId: selectedGame,
-      };
-      setPosts((prevPosts) => [...prevPosts, newPost]);
-      setPostContent("");
+
+    try {
+      const response = await fetch("http://localhost:3001/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text, userId, gameid, gamemodeid }),
+      });
+
+      if (response.ok) {
+        // El post se creó exitosamente, puedes realizar alguna acción aquí si es necesario
+        console.log("El post se creó exitosamente");
+      } else {
+        // Hubo un error al crear el post, puedes manejarlo de acuerdo a tus necesidades
+        console.error("Error al crear el post");
+      }
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
     }
   };
 
   return (
-    <div className="forum-container">
-      <div className="grid grid-cols-2 gap-8 mt-4">
-      <div>
-          <GamesBar onSelectGame={setSelectedGame} />
-        </div>
-        <div>
-          <div className="mt-8">
-            <form onSubmit={handlePostSubmit} className="flex">
-              <textarea
-                value={postContent}
-                onChange={handlePostContentChange}
-                placeholder="Escribe tu post..."
-                className="border border-gray-300 rounded-lg px-4 py-2 mr-2 w-full"
-              />
-              <button type="submit" className="bg-gray-800 text-white font-bold py-2 px-3 rounded-[2rem]">
-                Post
-              </button>
-            </form>
-          </div>
-          <div className="mt-8">
-            {posts
-              .filter((post) => post.gameId === selectedGame)
-              .map((post) => (
-                <div key={post.id} className="mb-2">
-                  <div className="flex-shrink-0">
-                    <img
-                      src="profile-pic.jpg"
-                      alt="Profile"
-                      className="w-10 h-10 rounded-full"
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-gray-800 font-bold">Username</p>
-                    <div className="bg-white rounded-lg p-2 shadow-md">{post.content}</div>
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
+    <div>
+      {/* ...otros elementos del componente GamePosts */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          placeholder="Ingrese el texto del post"
+        />
+        <button type="submit">Crear Post</button>
+      </form>
     </div>
   );
 };
