933d2cfb02ba6311a2511768241cb981569ba9d4
REVelazquez <124008724+REVelazquez@users.noreply.github.com>
2023-06-16 12:06:16 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         {location.pathname !== '/' && <NavBar />}
           <Routes>
             <Route path='/' element={<LandingPage/>} />
-            <Route path='/home' element={  <Chat/> }/>
+            <Route path='/home' element={  <Home/> }/>
             <Route path='/game/:id' element={<GamePosts />} />
             <Route path='/favorites' element={<Favorites/>} />
             <Route exact path='/login' element={ <Login/> }/>
