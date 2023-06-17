4dac6a743511033bc43ab90a964317953ecaf287
GgKallisti <riptheraven@gmail.com>
2023-06-17 16:25:07 -0300

    bueno, me cago en github

--- api/index.js
+++ api/index.js
@@ -2,15 +2,8 @@ const server = require ('./src/app.js')
 const {conn} = require ('./src/db.js')
 
 conn.sync({ force:false}).then (()=>{
-<<<<<<< Updated upstream
-    server.listen(3001, ()=>{
-        console.log('% listening at 3001')
-    })
-})
-=======
 
   server.listen(3001, ()=>{
       console.log('% listening at 3001')
   })
 })
->>>>>>> Stashed changes
