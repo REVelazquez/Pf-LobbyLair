a8c0b56d62466b007543470efca79ea8fb406ce7
matias arroyo <matiasarroyo684@gmail.com>
2023-06-20 16:40:05 -0300

    mercado pago ssl

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const https = require('https');
+const fs = require('fs');
+const options = {
+    key: fs.readFileSync('./localhost-key.pem'),
+    cert: fs.readFileSync('./localhost.pem')
+}
 
 https.createServer(options, server).listen(3001, ()=>{
     console.log('% listening at 3001')
