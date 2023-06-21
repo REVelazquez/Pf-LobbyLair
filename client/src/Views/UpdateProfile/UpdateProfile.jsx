4da1bd0c14d0b4321a3f75a61bfb2e4193214efb
Fervolpato <fervolpato1991@gmail.com>
2023-06-21 15:52:51 -0300

    actualizacion estilos update profile

--- client/src/Views/UpdateProfile/UpdateProfile.jsx
+++ client/src/Views/UpdateProfile/UpdateProfile.jsx
@@ -31,16 +31,16 @@ export default function UpdateProfile() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="min-h-screen bg-gray-100 pt-9 flex flex-col items-center justify-center">
       <form className="w-80 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-        <h2 className="text-2xl mb-4">Update Profile</h2>
+        <h2 className="text-xl font-bold mb-4 text-gray-800">Update Profile</h2>
         <div className="mb-4">
           <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Name</label>
           <input
             type="text"
             name="name"
             id="name"
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="w-full p-2 border border-gray-300 rounded-[5rem]"
             placeholder="Enter your name"
             value={formData.name}
             onChange={handleChange}
@@ -52,7 +52,7 @@ export default function UpdateProfile() {
             type="email"
             name="email"
             id="email"
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="w-full p-2 border border-gray-300 rounded-[5rem]"
             placeholder="Enter your email"
             value={formData.email}
             onChange={handleChange}
@@ -64,7 +64,7 @@ export default function UpdateProfile() {
             type="password"
             name="password"
             id="password"
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="w-full p-2 border border-gray-300 rounded-[5rem]"
             placeholder="Enter your password"
             value={formData.password}
             onChange={handleChange}
@@ -76,7 +76,7 @@ export default function UpdateProfile() {
             type="text"
             name="image"
             id="image"
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="w-full p-2 border border-gray-300 rounded-[5rem]"
             placeholder="Enter the image URL"
             value={formData.image}
             onChange={handleChange}
@@ -88,7 +88,7 @@ export default function UpdateProfile() {
             type="text"
             name="perfilUrl"
             id="perfilUrl"
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="w-full p-2 border border-gray-300 rounded-[5rem]"
             placeholder="Enter the perfil URL"
             value={formData.perfilUrl}
             onChange={handleChange}
@@ -109,7 +109,7 @@ export default function UpdateProfile() {
         <div className="flex items-center justify-center">
           <button
             type="submit"
-            className=" bg-[#1f2937] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className=" bg-black text-white border-none rounded-[5rem] p-3 text-l hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Update
           </button>
