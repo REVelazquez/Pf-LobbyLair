b4fd90630effe93bc238f5920a47afa8af68c310
matias arroyo <matiasarroyo684@gmail.com>
2023-06-21 11:18:55 -0300

    paypal backend

--- api/src/controllers/payPalController.js
+++ api/src/controllers/payPalController.js
@@ -1,53 +1,81 @@
 require('dotenv').config();
 const axios = require('axios');
-const {PAYPAL_API_SECRET,PAYPAL_API_CLIENT,PAYPAL_API} = process.env;
+const {PAYPAL_API_SECRET,PAYPAL_API_CLIENT,PAYPAL_API,CAPTURE_API} = process.env;
 const API = PAYPAL_API || 'https://api-m.sandbox.paypal.com';
-
-
+const API_CAPTURE = CAPTURE_API || 'https://api.sandbox.paypal.com';
 const createOrder = async (req, res) => {
-    const order = {
-        intent: 'CAPTURE',
-        purchase_units: [{
-            amount: {
-                currency_code: 'USD',
-                value: '5'
-            }
-        }],
-        application_context: {
-            brand_name: 'lobbylair',
-            landing_page: 'NO_PREFERENCE',
-            shipping_preference: 'NO_SHIPPING',
-            user_action: 'PAY_NOW',
-            return_url: 'https://localhost:3001/capture-order',
-            cancel_url: 'https://localhost:3001/cancel-order',
-        },
+  const order = {
+    intent: 'CAPTURE',
+    purchase_units: [{
+      amount: {
+        currency_code: 'USD',
+        value: '5'
       }
-        const params = new URLSearchParams(); 
-        params.append('grant_type', 'client_credentials');
-        const {data:{access_token}} = await axios.post(API + '/v1/oauth2/token', params , {
-          auth:{
-            username: PAYPAL_API_CLIENT,
-            password: PAYPAL_API_SECRET,
-          }
-        })
-      const response = await axios.post(API + '/v2/checkout/orders', order, {
-        Authorization: `Bearer ${access_token}`,
-      })
-      console.log(response);
-      return res.json('captured order');
+    }],
+    application_context: {
+      brand_name: 'lobbylair',
+      landing_page: 'NO_PREFERENCE',
+      shipping_preference: 'NO_SHIPPING',
+      user_action: 'PAY_NOW',
+      return_url: 'http://localhost:3001/capture-order',
+      cancel_url: 'http://localhost:3001/cancel-order',
+    },
+  };
+
+  const params = new URLSearchParams();
+  params.append('grant_type', 'client_credentials');
+
+  const authString = `${PAYPAL_API_CLIENT}:${PAYPAL_API_SECRET}`;
+  const encodedAuth = Buffer.from(authString).toString('base64');
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: `Basic ${encodedAuth}`,
+  };
+
+  try {
+    const { data: { access_token } } = await axios.post(
+      `${API}/v1/oauth2/token`,
+      params,
+      { headers }
+    );
+      console.log(access_token);
+    const response = await axios.post(
+      `${API}/v2/checkout/orders`,
+      order,
+      { headers: { Authorization: `Bearer ${access_token}` } }
+    );
+
+    console.log(response.data);
+    return res.json('captured order');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json('Failed to create order');
+  }
 };
 
 const captureOrder = async (req, res) => {
-  const {token} = req.body;
-  const response = await axios.post(API + '/v2/checkout/orders/'+ token +'/capture',{}, {
-    auth:{
-      username: PAYPAL_API_CLIENT,
-      password: PAYPAL_API_SECRET,
-    }
-  });
-  console.log(response.data);
-  return res.send('payed');
+  const { token } = req.body;
+  console.log(req.body);
+  try {
+    const response = await axios.post(
+      `${API_CAPTURE}/v2/checkout/orders/${token}/capture`,
+      {},
+      {
+        auth: {
+          username: PAYPAL_API_CLIENT,
+          password: PAYPAL_API_SECRET,
+        },
+      }
+    );
+    
+    console.log(response.data);
+    return res.send('payed');
+  } catch (error) {
+    console.error(error.response.data);
+    return res.status(500).json('Failed to capture order');
   }
+};
+
 
 
 const cancelPayment = (req, res) => {
