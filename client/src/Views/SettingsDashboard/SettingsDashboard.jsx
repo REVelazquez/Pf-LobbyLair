90dbe73bf71a451175844da0dfa37a31225ae685
YeisonR05 <yeijoh91@gmail.com>
2023-06-26 16:48:35 -0500

    3 Nuevas rutas para el Dashboard

---
+++ client/src/Views/SettingsDashboard/SettingsDashboard.jsx
@@ -0,0 +1,9 @@
+const SettingsDashboard = () => {
+    return (
+        <div>
+            Aquí se puede agregar o eliminar un usuario o un admi y demás
+        </div>
+    )
+};
+
+export default SettingsDashboard;
---
+++ client/src/Views/UsersDashboard/UsersDashboard.jsx
@@ -0,0 +1,9 @@
+const UsersDashboard = () => {
+    return (
+        <div>
+            Aquí debe ir lo de User
+        </div>
+    )
+};
+
+export default UsersDashboard;
