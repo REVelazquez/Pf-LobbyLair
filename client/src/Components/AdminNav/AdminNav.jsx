ecc787591914429d0a5610549ddd1be7e0a60673
YeisonR05 <yeijoh91@gmail.com>
2023-06-27 00:23:04 -0500

    AdmiNav y vistas de la Dashboard con estilos

--- client/src/Components/AdminNav/AdminNav.jsx
+++ client/src/Components/AdminNav/AdminNav.jsx
@@ -1,17 +1,28 @@
 import { useNavigate } from "react-router-dom";
 
-
 const AdminNav = ()=>{
     const navigate=useNavigate()
 
 
    return( <nav className="">
-    <div className="ml-96 mr-96 max-h-14 bg-[#1f2937]">
+    <div className="justify-center m-2 mx-auto items-center mt-6 p-2 w-[90%]  flex gap-20 bg-gray-200 rounded-[2rem] shadow-2xl">
 
-        <button className="ml-12 mr-12 mt-2 mb-4 font-bold text-white hover:text-gray-300 " onClick={()=>navigate('/admindashboard/dataset')}>Dataset</button>
-        <button className="ml-12 mr-12 mt-2 mb-4 font-bold text-white hover:text-gray-300 " onClick={()=>navigate('/admindashboard/settings')}>AdminSettings</button>
-        <button className="ml-12 mr-12 mt-2 mb-4 font-bold text-white hover:text-gray-300 " onClick={()=>navigate('/admindashboard')}>Statistics</button>
-        <button className="ml-12 mr-12 mt-2 mb-4 font-bold text-white hover:text-gray-300 " onClick={()=>navigate('/admindashboard/users')}>Users</button>
+        <button className="justify-center m-1 py-1.5 px-10 border border-transparent hover:text-black rounded-md shadow-sm text-base font-medium text-white bg-gray-800 hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2" 
+                onClick={()=>navigate('/admindashboard/dataset')}>
+                Dataset
+        </button>
+        <button className="justify-center m-1 py-1.5 px-10 border border-transparent hover:text-black rounded-md shadow-sm text-base font-medium text-white bg-gray-800 hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2" 
+                onClick={()=>navigate('/admindashboard/settings')}>
+                AdminSettings
+        </button>
+        <button className="justify-center m-1 py-1.5 px-10 border border-transparent hover:text-black rounded-md shadow-sm text-base font-medium text-white bg-gray-800 hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2" 
+                onClick={()=>navigate('/admindashboard')}>
+                Statistics
+        </button>
+        <button className="justify-center m-1 py-1.5 px-10 border border-transparent hover:text-black rounded-md shadow-sm text-base font-medium text-white bg-gray-800 hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2" 
+                onClick={()=>navigate('/admindashboard/users')}>
+                Users
+        </button>
     </div>
     </nav>)
 }
