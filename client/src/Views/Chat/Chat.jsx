ef089adf487ff69ebc8cb3f3d692069085231490
matias arroyo <matiasarroyo684@gmail.com>
2023-06-28 10:35:50 -0300

    chat

--- client/src/Views/Chat/Chat.jsx
+++ client/src/Views/Chat/Chat.jsx
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { PrettyChatWindow } from 'react-chat-engine-pretty';
-const Chat = () => {
-  return (
-    <div style={{ height: '100vh' }}>
-      <PrettyChatWindow
-      projectId="f390f109-520d-4265-9316-21c8428c65e4"
-      username="john_smith"
-      secret="pass1234"
-      style={{ height: '100vh' }}
-    />
-    </div>
-  )
-}
-
-export default Chat
-=======
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 import axios from "axios";
 import { useSelector } from "react-redux";
@@ -37,6 +19,4 @@ const ChatsPage = async (props) => {
     </div>
   );
 };
-
 export default ChatsPage;
->>>>>>> 8e73529745af6abdfe5e8fe48c0bac8a190f63d2
