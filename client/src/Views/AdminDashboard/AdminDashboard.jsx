e2b93cab4d89858e6be6ebd3c54a7e68c5f25682
Fervolpato <fervolpato1991@gmail.com>
2023-06-30 14:00:12 -0300

    datos en grafico de barras y arreglo register

--- client/src/Views/AdminDashboard/AdminDashboard.jsx
+++ client/src/Views/AdminDashboard/AdminDashboard.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Pie, Line, Bar } from 'react-chartjs-2';
 import { Chart, registerables } from 'chart.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllFavorites, getAllGames } from '../../Redux/actions';
 Chart.register(...registerables);
 
 const lineChartData = {
@@ -31,7 +33,7 @@ const barChartData = {
   labels: ['Valorant', 'Minecraft', 'Diablo4', 'Gta 5 Online', 'League of Legends', 'Apex Legends'],
   datasets: [
     {
-      label: 'Posts',
+      label: 'Número de favoritos',
       data: [12, 19, 3, 5, 2, 3],
       backgroundColor: '#FF6384',
     },
@@ -43,6 +45,41 @@ const options = {
 };
 
 const AdminDashboard = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllFavorites());
+    dispatch(getAllGames());
+  }, []);
+
+  const games = useSelector(state => state.games);
+  const counts = useSelector(state => state.favoritesCount);
+
+  const newObject = counts.map(count => {
+    const game = games.find(game => +game.id === +count.gameId);
+    const name = game ? game.name : null;
+    return {
+      ...count,
+      name: name
+    };
+  });
+
+  // Obtener los nombres de los juegos y los recuentos de newObject
+  const labels = newObject.map(item => item.name);
+  const data = newObject.map(item => item.count);
+
+  // Actualizar los valores de labels y data en barChartData
+  const updatedBarChartData = {
+    ...barChartData,
+    labels: labels,
+    datasets: [
+      {
+        ...barChartData.datasets[0],
+        data: data
+      }
+    ]
+  };
+
   return (
     <div className="flex flex-col w-full md:flex-row">
       <div className="w-full md:w-1/3 mt-5">
@@ -55,12 +92,11 @@ const AdminDashboard = () => {
           <Line data={lineChartData} options={options} />
         </div>
         <div className="w-full md:w-1/2">
-          <h1 className="text-center my-4">Estadísticas de Post en Juegos</h1>
-          <Bar data={barChartData} options={options} />
+          <h1 className="text-center my-4">Estadísticas de Juegos Favoritos</h1>
+          <Bar data={updatedBarChartData} options={options} />
         </div>
       </div>
     </div>
   );
 };
-
 export default AdminDashboard;
